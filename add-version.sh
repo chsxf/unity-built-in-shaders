b8549caa4e24c0a49477147dc60ef09afe724a19
Christophe SAUVEUR <christophe@cheeseburgames.com>
2019-06-06 01:28:54 +0200

    Added new version import script

---
+++ add-version.sh
@@ -0,0 +1,43 @@
+if [ "$#" -ne 1 ]; then
+	echo "Usage: add-version url"
+	exit
+fi
+
+URL=$1
+
+if [[ $URL =~ builtin_shaders-(.+)\.zip$ ]]; then
+	VERSION=${BASH_REMATCH[1]}
+else
+	echo "Invalid URL"
+	exit
+fi
+
+ZIP_FILENAME="builtin_shaders-${VERSION}.zip"
+
+if [[ $VERSION =~ ^([0-9]+\.[0-9]+)\. ]]; then
+	MAJOR_BRANCH=${BASH_REMATCH[1]}
+fi
+
+LOCAL_BRANCH_COUNT=`git branch --list | grep "$MAJOR_BRANCH" | wc -l`
+REMOTE_BRANCH_COUNT=`git branch -r | grep "origin/${MAJOR_BRANCH}" | wc -l`
+
+if [ $LOCAL_BRANCH_COUNT -eq 1 ]; then
+	git checkout $MAJOR_BRANCH
+elif [ $REMOTE_BRANCH_COUNT -eq 1 ]; then
+	git checkout "origin/${MAJOR_BRANCH}" 
+else
+	git checkout -b $MAJOR_BRANCH
+fi
+
+wget $URL
+unzip $ZIP_FILENAME
+rm $ZIP_FILENAME
+
+COMMIT_MESSAGE="Version ${VERSION}"
+TAG_MESSAGE="${COMMIT_MESSAGE}"
+
+git add --all
+git commit -m "${COMMIT_MESSAGE}"
+git push --set-upstream origin "${MAJOR_BRANCH}"
+git tag -a "v${VERSION}" -m "${TAG_MESSAGE}"
+git push --tags
